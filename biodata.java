6a6d14d08c97cdac7dbf9f32fe9a884a5efbd32d
Dian80 <70132205+Dian80@users.noreply.github.com>
2020-10-16 11:11:08 +0700

    Add files via upload

---
+++ biodata.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ENDANG
+ */
+public class biodata {
+    public void infoNama(){
+        System.out.println("Nama : Dian Anggraini");
+    }
+    
+    public void infoJurusan(){
+        System.out.println("Jurusan : XI RPL 1");
+    }
+    
+    public void infoTTL(){
+        System.out.println("TTL: 24 Februari 2004 ");
+    }
+    
+    public void infoAlamat(){
+        System.out.println("Alamat : Jl.Panditorejo Tumpuk-Sambisirah-Wonorejo-Pasuruan");
+    }
+   
+    public void infoHobi(){
+        System.out.println("Hobi : NgeDrakor");
+    }
+}
+    
